044ab144f55afd1903cdead8a4b7fe24c32ba8c1
Jethro Dean <jethroldean@gmail.com>
2022-03-29 15:59:19 +1100

    Passing values other than strings via props

--- src/App.js
+++ src/App.js
@@ -7,17 +7,33 @@ export default function App() {
             <Joke 
                 setup="1. I bought a thesaurus today and when I got it home I found out all the pages were blank."
                 punchline="I have no words to describe how annoyed I am."
+                upvotes={2}
+                downvotes={3}
+                comments={["lame, great"]}
+                isPun={false}
             />
             <Joke 
                 setup="2. I went to a really emotional wedding the other day."
                 punchline="Even the cake was in tiers."
+                upvotes={4}
+                downvotes={5}
+                comments={["lame, great"]}
+                isPun={true}
             />
             <Joke 
                 setup="3. Did you hear about the ATM that got addicted to money?"
                 punchline="It suffered from withdrawals."
+                upvotes={2}
+                downvotes={1}
+                comments={["lame, great"]}
+                isPun={true}
             />
             <Joke 
                 punchline="4. Never trust atoms, they make up everything."
+                upvotes={12}
+                downvotes={3}
+                comments={["lame, great"]}
+                isPun={true}
             />
         </div>
 
--- src/Joke.js
+++ src/Joke.js
@@ -5,6 +5,10 @@ export default function Joke(props) {
         <div>
             {props.setup && <p>{props.setup}</p>}
             <p className="joke--punchline">{props.punchline}</p>
+            <p>{props.upvotes}</p>
+            <p>{props.downvotes}</p>
+            <p>{props.comments}</p>
+            <p>{props.isPun}</p>
         </div>
     )
 }
